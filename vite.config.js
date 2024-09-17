1a3c50afab1fc942e87a9baf7387d5022a5e8300
ramo2004 <omarimian@gmail.com>
2024-09-17 15:38:08 -0500

    Add vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    resolve: {
+        alias: {
+            'three': 'three'
+        }
+    },
+    optimizeDeps: {
+        include: ['three']
+    }
+})
